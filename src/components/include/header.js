78dc0436c791c315e46c0b9acad6a5044128cab0
AlirezaAbedi <aabedi79@gmail.com>
2020-11-29 18:28:24 +0330

    Apply Bootstrap Design

---
+++ src/components/include/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Header extends Component {
+    state = {}
+    render() {
+        return (
+            <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">React GoJS</a>
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            </nav>
+        );
+    }
+}
+
+export default Header;
